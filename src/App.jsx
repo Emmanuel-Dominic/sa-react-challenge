e9592b0add2006c6086ceb49594ffca8d3d34a43
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2025-03-09 18:23:10 +0300

    implements initial data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
+import { REACT_APP_BASE_URL } from "./utils/index";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const fetchUsers = async() => {
+        try{
+            const response = await axios.get(REACT_APP_BASE_URL);
+            setUsers(response.data);
+            setLoading(false);
+        }catch(err){
+            setError("Failed to fetch users!");
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, [])
+
+    if(loading) return <div>Loading...</div>;
+    if(error) return <div>{error}</div>;
+
+    return (
+        <div className="App">
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>No:</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    { users.length > 0 ? users.map((user, index) => (
+                        <tr key={index}>
+                            <td>{index+1}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.phone}</td>
+                            <td>
+                                <button type="button">Edit</button>
+                                <button type="button">Delete</button>
+                            </td>
+                        </tr>
+                    )) : (
+                        <tr colSpan="5">
+                            <td>
+                              <h4>No users found!</h4>
+                            </td>
+                        </tr>
+                    )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
 }
 
 export default App
